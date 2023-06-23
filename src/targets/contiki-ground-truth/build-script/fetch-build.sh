66cb3d1e9d4844dea5734e14c933398f4cc71fef
Kyle Robinson <robin489@purdue.edu>
2023-06-23 17:02:08 +0000

    Tweaked contiki fetch and build to use my contiki changes and replace the makefile

--- src/targets/contiki-ground-truth/build-script/fetch-build.sh
+++ src/targets/contiki-ground-truth/build-script/fetch-build.sh
@@ -6,7 +6,7 @@ set -e
 #COMMIT_TO_FUZZ
 
 ## install contiki-ng
-git clone https://github.com/contiki-ng/contiki-ng.git ${CONTIKI_PATH}
+git clone https://github.com/krobinson395/contiki-ng.git ${CONTIKI_PATH}
 ## commit version to checkout
 if [ -z "${COMMIT_TO_FUZZ}" ];
 then
@@ -14,3 +14,9 @@ then
 else
   git -C ${CONTIKI_PATH} checkout ${COMMIT_TO_FUZZ}
 fi
+
+git clone https://github.com/xzwDavid/makefile.git ${SOFTWARE_PATH}/fix_for_makefile
+rm ${CONTIKI_PATH}/arch/cpu/native/Makefile.native
+rm ${CONTIKI_PATH}/os/net/ipv6/uip6.c
+cp ${SOFTWARE_PATH}/fix_for_makefile/Makefile.native ${CONTIKI_PATH}/arch/cpu/native/Makefile.native
+cp ${SOFTWARE_PATH}/fix_for_makefile/uip6.c ${CONTIKI_PATH}/os/net/ipv6/ip6.c
